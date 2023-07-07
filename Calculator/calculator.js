05c2d5099e8cdf338883bed60895eaf60c53b516
Mani <128119718+ManiM97@users.noreply.github.com>
2023-07-07 12:47:39 +0530

    Add files via upload

---
+++ Calculator/calculator.js
@@ -0,0 +1,30 @@
+let displayElement = document.getElementById('display');
+
+let buttonsElement = Array.from(document.getElementsByClassName('button'));
+
+
+buttonsElement.map( button => {
+    button.addEventListener('click',(e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if (display.innerText){
+                    display.innerText = display.innerText.slice(0,-1);
+                }
+                break; 
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                }
+                catch{
+                    display.innerText = 'Error!';
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+                
+        }
+    });
+});
